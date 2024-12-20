3e707d331e5140b195b89ef06d4d79abd67786dc
ThaiAn1809 <buithaian321@gmail.com>
2024-12-20 20:32:39 +0700

    a

--- database.py
+++ database.py
@@ -1,5 +1,11 @@
 import sqlite3
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 def create_user(email, name, password):
     conn = sqlite3.connect('./data/database.db')
     cursor = conn.cursor()
@@ -10,7 +16,8 @@ def create_user(email, name, password):
 def find_user_by_email(email):
     conn = sqlite3.connect('./data/database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT email, name, password FROM user WHERE email = ?', (email, ))
+    cursor.row_factory = dict_factory
+    cursor.execute('SELECT id, email, name, password FROM user WHERE email = ?', (email, ))
     user = cursor.fetchone()
     conn.close()
     return user
@@ -18,7 +25,8 @@ def find_user_by_email(email):
 def find_user_by_email_and_password(email, password):
     conn = sqlite3.connect('./data/database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT email, name, password FROM user WHERE email = ? AND password = ?', (email, password))
+    cursor.row_factory = dict_factory
+    cursor.execute('SELECT id, email, name, password FROM user WHERE email = ? AND password = ?', (email, password))
     user = cursor.fetchone()
     conn.close()
     return user
